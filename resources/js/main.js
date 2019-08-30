6bf59c6af0485112a963a32e1d0671e74d145f55
Gabriel Fernandes Giraud <gabriel.giraud@fatec.sp.gov.br>
2019-08-30 20:22:10 -0300

    Adicionando algumas configs do angularjs

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,12 +1,4 @@
-var app = angular.module("fatecApp", []); 
-  $(function () {
-  $('[data-toggle="popover"]').popover()
-  $('[data-toggle="tooltip"]').tooltip()})
-app.controller("loginCtrl", function($scope) {
-
-	$scope.definirCorBotao = (user) => definirCorBotao(user);
-
-	function definirCorBotao (user) {
-		return user.usuario !=null && user.senha.length >=6 ? 'btn-success' : 'btn-danger';
-	};
-});
+$(function () {
+$('[data-toggle="popover"]').popover()
+$('[data-toggle="tooltip"]').tooltip()})
+  
---
+++ routes.js
@@ -0,0 +1,16 @@
+app.config(function($routeProvider) {
+    $routeProvider
+    .when("/", {
+      templateUrl : "home/home.html"
+    })
+    .when("/admin", {
+      templateUrl : "admin/admin.html"
+    })
+    .when("/escola", {
+      templateUrl : "escola/escola.html"
+    })
+    .when("/login", {
+      templateUrl : "login/login.html"
+    })
+    .otherwise({redirectTo:'/'})
+})
