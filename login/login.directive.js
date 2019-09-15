108dafe2ef31c40985aed628922e451688929be7
gabrielmulticontas <gabriel@multicontas.com>
2019-09-15 16:25:16 -0300

    #CHANGE: Mudanças na tela de empresario

--- login/login.directive.js
+++ login/login.directive.js
@@ -1,5 +1,7 @@
 app.directive('login', function () {
     return {
-        templateUrl: './login/login.html'
+        templateUrl: './login/login.html',
+        controller:'loginCtrl',
+        controllerAs:'ctrl'
     };
 });
