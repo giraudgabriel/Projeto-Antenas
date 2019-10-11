423d0a104d5caf0f40b8b4b0827e77ead3a2ca1a
Felipe Nicoletti Reis Mario <felipenicolettirmario@gmail.com>
2019-10-11 20:22:55 -0300

    #CHANGES: Adicionado atributos e métodos

--- backend/CADI.java
+++ backend/CADI.java
@@ -1,4 +1,16 @@
 public class CADI
 {
-    private String 
+    private String email;
+    private String senha;
+
+    public CADI(String email,String senha)
+    {
+        this.email = email;
+        this.senha = senha;
+    }
+
+    public void setSenha(String novaSenha)
+    {
+        this.senha = novaSenha;
+    }
 }
