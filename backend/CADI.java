68f8a743bf383e2baec7c33610c4a6e00f10fa35
Felipe Nicoletti Reis Mario <felipenicolettirmario@gmail.com>
2019-10-13 22:35:40 -0300

    #FEATURES: Adicionado novas funções do CADI

--- backend/CADI.java
+++ backend/CADI.java
@@ -3,14 +3,23 @@ public class CADI
     private String email;
     private String senha;
 
-    public CADI(String email,String senha)
-    {
+    public CADI(String email,String senha) {
         this.email = email;
         this.senha = senha;
     }
 
-    public void setSenha(String novaSenha)
-    {
+    public void setSenha(String novaSenha){
         this.senha = novaSenha;
     }
+    public void setemail(String novoEmail){
+        this.email = novoEmail;
+    }
+    public getEmail(){
+        return email;
+    }
+    public getSenha(){
+        return senha;
+    }
+
+
 }
