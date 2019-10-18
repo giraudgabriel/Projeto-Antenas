583942616337decef2037890f318e39dbd510e33
Felipe Nicoletti Reis Mario <felipenicolettirmario@gmail.com>
2019-10-18 20:07:28 -0300

    #ADD: Arquivos construidos na aula pelo professor

---
+++ backend/Projeto.java
@@ -0,0 +1,25 @@
+package antenas;
+
+public class Projeto {
+
+	private String chave;
+	//acrescentar todos os demais atributos do projeto que foram passados na fase de requisitos
+
+	public Projeto(String chave) {
+		this.chave = chave;
+	}
+
+	public String getChave() {
+		return chave;
+	}
+
+	public void setChave(String chave) {
+		this.chave = chave;
+	}
+
+	
+	
+	
+	
+	
+}
