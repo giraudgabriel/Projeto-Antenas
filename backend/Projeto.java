fe830198c93bc2d0cdbcc58a49f8364de6f1f774
Felipe Nicoletti Reis Mario <felipenicolettirmario@gmail.com>
2019-10-21 19:10:03 -0300

    #FEATURES: Adicionado novas funções e atributos.

--- backend/Projeto.java
+++ backend/Projeto.java
@@ -3,10 +3,16 @@ package antenas;
 public class Projeto {
 
 	private String chave;
+	private String dono;
+	private String smallDesc;
+	private String externalLink;
 	//acrescentar todos os demais atributos do projeto que foram passados na fase de requisitos
 
-	public Projeto(String chave) {
+	public Projeto(String chave,String dono,String smallDesc,String externalLink) {
 		this.chave = chave;
+		this.dono = dono;
+		this.externalLink = externalLink;
+		this.smallDesc = smallDesc;
 	}
 
 	public String getChave() {
@@ -16,6 +22,28 @@ public class Projeto {
 	public void setChave(String chave) {
 		this.chave = chave;
 	}
+
+	public String getDono(){
+		return dono;
+	}
+
+	public String getSD(){
+		return smallDesc;
+	}
+
+	public void editSD(String NewSD){
+		this.smallDesc = NewSD;
+	}
+
+	public String getLink()
+	{
+		return externalLink;
+	}
+
+
+	
+	
+
 
 	
 	
